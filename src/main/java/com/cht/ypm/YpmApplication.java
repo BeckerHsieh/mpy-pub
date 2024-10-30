f83d3c40ede9639959fff016e71d8bffb1bf0c6d
BeckerHsieh <beckerhsieh@cht.com.tw>
2024-10-30 16:38:17 +0800

    first commit

---
+++ src/main/java/com/cht/ypm/YpmApplication.java
@@ -0,0 +1,13 @@
+package com.cht.ypm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YpmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cht/ypm/YpmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cht.ypm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YpmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
